dd9c8dfeba4db33cc2602658db8317e7c3afbbd6
Andres Calvo Ariza <andres.calvo@simetrik.com>
2024-10-02 10:50:16 -0500

    Add Geo Localization test with esp32

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,17 @@
 #include <WiFiClientSecure.h>
 #include <MQTTClient.h>
 #include <ArduinoJson.h>
+#include <TinyGPS++.h>
 #include "WiFi.h"
+
+#define RXD2 16
+#define TXD2 17
+#define MONITOR_SPEED 9600
+
 // put function declarations here:
+
+TinyGPSPlus gps;
+HardwareSerial gpsSerial(2);
 
 /**
  * Para modificar la conexion a internet ir al archivo secrets.hpp
@@ -12,7 +21,7 @@
  */
 
 /**
- * Inicio del Setup de Conexion a AWS IoT Core 
+ * Inicio del Setup de Conexion a AWS IoT Core
  * No toca modificar nada aquí
  */
 // The MQTT topics that this device should publish/subscribe
@@ -25,7 +34,8 @@ void connectAWS()
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -38,11 +48,13 @@ void connectAWS()
   // Create a message handler
   // client.onMessage(messageHandler);
   Serial.print("Connecting to AWS IOT");
-  while (!client.connect(THINGNAME)) {
+  while (!client.connect(THINGNAME))
+  {
     Serial.print(".");
     delay(100);
   }
-  if(!client.connected()){
+  if (!client.connected())
+  {
     Serial.println("AWS IoT Timeout!");
     return;
   }
@@ -51,8 +63,9 @@ void connectAWS()
   Serial.println("AWS IoT Connected!");
 }
 
-void publishMesssage(float speed,float lat,float lng){
-  //See https://arduinojson.org/
+void publishMesssage(float speed, float lat, float lng)
+{
+  // See https://arduinojson.org/
   JsonDocument doc;
   doc["speed"] = speed;
   doc["lat"] = lat;
@@ -60,37 +73,49 @@ void publishMesssage(float speed,float lat,float lng){
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to mqtt_client
 
-  //Publish to the topic
+  // Publish to the topic
   client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
   Serial.println("Sent a message");
 }
 /**
- * Fin del Setup de Conexión a AWS IoT Core 
+ * Fin del Setup de Conexión a AWS IoT Core
  */
 
 /**
  * A partir de aqui inyecten todo el codigo que tenian antes de los sensores
  */
 
-
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+  Serial.begin(MONITOR_SPEED);
+  // Start Serial 2 with the defined RX and TX pins and a baud rate of 9600
+  gpsSerial.begin(MONITOR_SPEED, SERIAL_8N1, RXD2, TXD2);
   connectAWS();
 }
 
+void loop()
+{
+  while (gpsSerial.available() > 0)
+  {
+    gps.encode(gpsSerial.read());
+  }
 
+  if (gps.location.isUpdated())
+  {
+    /**
+     * Ajustar para colocar los valores de los sensores en estas tres variables
+     */
+    float speed = 0; // valor resultado de la medicion de la velocidad
+    float lat = gps.location.lat();
+    float lng = gps.location.lng();
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  /**
-   * Ajustar para colocar los valores de los sensores en estas tres variables
-   */
-  float speed = 0; // valor resultado de la medicion de la velocidad
-  float lat = 0;
-  float lng = 0;
+    Serial.print("Latitud = ");
+    Serial.println(lat, 6);
+    Serial.print("Longitud = ");
+    Serial.println(lng, 6);
+    publishMesssage(speed, lat, lng);
+    client.loop();
+  }
 
-  publishMesssage(speed,lat,lng);
-  client.loop();  
   delay(1000);
 }
-
